dc0232e3875701db1520ead4246336b661a84c58
weirong.zhang <weirong.zhang@beebank.com>
2016-09-03 12:52:22 +0800

    init

---
+++ src/main/java/ind/netty/NettyClientBootstrap.java
@@ -0,0 +1,68 @@
+package ind.netty;
+
+import java.util.concurrent.TimeUnit;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.timeout.IdleStateHandler;
+
+public class NettyClientBootstrap {
+
+    /*
+     * 服务器端口号
+     */
+    private int port;
+
+    /*
+     * 服务器IP
+     */
+    private String host;
+
+    public NettyClientBootstrap(int port, String host)
+            throws InterruptedException {
+        this.port = port;
+        this.host = host;
+        start();
+    }
+
+    private void start() throws InterruptedException {
+
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+
+        try {
+
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(eventLoopGroup);
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.option(ChannelOption.SO_KEEPALIVE, true);
+            bootstrap.remoteAddress(host, port);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel socketChannel) throws Exception {
+                    socketChannel.pipeline().addLast(new NettyClientHandler());
+                }
+            });
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+            if (future.isSuccess()) {
+                SocketChannel socketChannel = (SocketChannel) future.channel();
+                System.out.println("----------------connect server success----------------");
+            }
+            future.channel().closeFuture().sync();
+        } finally {
+            eventLoopGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        NettyClientBootstrap client = new NettyClientBootstrap(9999, "localhost");
+
+    }
+}
---
+++ src/main/java/ind/netty/NettyClientHandler.java
@@ -0,0 +1,35 @@
+package ind.netty;
+
+import java.nio.charset.Charset;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+public class NettyClientHandler extends ChannelHandlerAdapter {
+    private ByteBuf firstMessage;
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        byte[] data = "服务器，给我一个APPLE".getBytes();
+        firstMessage = Unpooled.buffer();
+        firstMessage.writeBytes(data);
+        ctx.writeAndFlush(firstMessage);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        String rev = getMessage(buf);
+        System.out.println("客户端收到服务器数据:" + rev);
+    }
+
+    private String getMessage(ByteBuf buf) {
+        byte[] con = new byte[buf.readableBytes()];
+        buf.readBytes(con);
+
+        return new String(con, Charset.forName("Utf-8"));
+
+    }
+}
